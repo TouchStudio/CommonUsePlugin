68c310bc983a4842665a8df3ea5c762d8bf41983
XuFang <871271203@qq.com>
2024-07-30 22:43:34 +0800

    create

---
+++ src/main/java/top/touchstudio/cup/CommonUsePlugin.java
@@ -0,0 +1,23 @@
+package top.touchstudio.cup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @Autho TouchStudio
+ * @Github https://github.com/TouchStudio
+ * @Date 2024-07-30 22:00
+ */
+
+public final class CommonUsePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
