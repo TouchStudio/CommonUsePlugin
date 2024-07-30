a92155a0a2f69db2ebff853d5cc587e81651b241
XuFang <871271203@qq.com>
2024-07-31 03:57:42 +0800

    add NightVisionCommand.java[夜视]

--- src/main/java/top/touchstudio/cup/modules/quit/QuitCommand.java
+++ src/main/java/top/touchstudio/cup/modules/quit/QuitCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class Quit implements CommandExecutor {
+public class QuitCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player && command.getName().equalsIgnoreCase("quit")) {
--- src/main/java/top/touchstudio/cup/modules/sneakspeedtree/SneakSpeedTreeListener.java
+++ src/main/java/top/touchstudio/cup/modules/sneakspeedtree/SneakSpeedTreeListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
-public class SneakSpeedTree implements Listener {
+public class SneakSpeedTreeListener implements Listener {
 
     @EventHandler
     public void onPlayerToggleSneak(PlayerToggleSneakEvent e){
